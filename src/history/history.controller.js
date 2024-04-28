59f3775de85b8b730b87cf76ad722893ef231994
tharreo <s22110546@student.unklab.ac.id>
2024-04-28 22:19:50 +0800

    adding history

---
+++ src/history/history.controller.js
@@ -0,0 +1,105 @@
+const express = require('express')
+const prisma = require('../db')
+const router = express.Router();
+
+const { getAllHistorys, getHistoryById, createHistory, deleteHistoryById, ubdateHistoryById } = require("./history.service")
+
+router.get("/", async (req, res) => {
+    try {
+        const historys = await getAllHistorys()
+
+        res.status(200).json({
+            status: "success get the database",
+            data: historys,
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Internal Server Error");
+    }
+});
+
+router.post("/", async (req, res) => {
+    const newHistorysData = req.body
+    const history = await createHistory(newHistorysData)
+    res.send({
+        data: history,
+        message: "Data berhasil dimasukan"
+    })
+});
+
+router.delete("/:id", async (req, res) => {
+    try {
+        const historyId = (req.params.id)
+        await deleteHistoryById(parseInt(historyId))
+
+        res.status(200).json({
+            status: "success",
+            message: "Data berhasil dihapus",
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("tidak ada data");
+    }
+});
+
+router.get("/:id", async (req, res) => {
+    const historyId = parseInt(req.params.id);
+    try {
+        const historys = await getHistoryById(parseInt(historyId));
+
+        res.status(200).json({
+            status: "data berhasil didapatkan",
+            data: historys,
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("tidak ada data");
+    }
+});
+
+router.put("/:id", async (req, res) => {
+    const historyId = req.params.id;
+    const historyData = req.body;
+
+    if (
+        !(
+            historyData.tanggal_transaksi &&
+            historyData.nama_produk &&
+            historyData.jenis_produk &&
+            historyData.product &&
+            historyData.deskripsi
+        )
+    ) {
+        return res.status(400).send("Some fields are missing")
+    }
+    try {
+        const history = await ubdateHistoryById(parseInt(historyId), historyData)
+           
+        res.send({
+            message: "Data berhasil diperbarui",
+            data: history,
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Internal Server Error");
+    }
+});
+
+router.patch("/:id", async (req, res) => {
+    const historyId = req.params.id;
+    const historyData = req.body;
+
+    try {
+        const history = await ubdateHistoryById(parseInt(historyId), historyData)
+           
+        res.send({
+            message: "Data berhasil diperbarui",
+            data: history,
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Internal Server Error");
+    }
+});
+
+module.exports = router
---
+++ src/history/history.repository.js
@@ -0,0 +1,75 @@
+const prisma = require("../db")
+
+const findHistorys = async () => {
+    const historys = await prisma.history.findMany()
+    return historys
+}
+
+const findHistorysById = async (id) => {
+    const historys = await prisma.history.findUnique({
+        where:{
+            id,
+        }
+    })
+    return historys
+}
+
+const insertHistory = async (newHistorysData) => {
+    try {
+        const history = await prisma.history.create({
+            data: {
+                tanggal_transaksi: new Date(), 
+                nama_produk: newHistorysData.nama_produk,
+                jenis_produk: newHistorysData.jenis_produk,
+                product: {
+                  connect: {
+                    id: newHistorysData.productId 
+                  }
+                },
+                deskripsi: newHistorysData.deskripsi
+              }
+              
+        });
+        return history;
+    } catch (error) {
+        console.error(error);
+        throw new Error("Gagal membuat produk");
+    }
+}
+
+
+const deleteHistory = async (id) =>{
+     await prisma.history.delete({
+        where: {
+            id,
+        },
+    });
+}
+
+const ubdateHistory = async (id, historyData) => {
+    const history = await prisma.history.update({
+        where: {
+            id: parseInt(id),
+        },
+        data: {
+            tanggal_transaksi: new Date(), 
+            nama_produk: historyData.nama_produk,
+            jenis_produk: historyData.jenis_produk,
+            product: {
+              connect: {
+                id: historyData.productId
+              }
+            },
+            deskripsi: historyData.deskripsi
+          },
+    });
+    return history
+}
+
+module.exports={
+    findHistorys,
+    findHistorysById,
+    insertHistory,
+    deleteHistory,
+    ubdateHistory
+}
